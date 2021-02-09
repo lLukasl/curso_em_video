f82d99f59ceae3ee2f1bcc5497c97b2031a6ec23
Lucas Dias de Miranda <lucas.miranda@pampa.compasso>
2021-02-09 15:13:49 -0300

    tabuada

---
+++ aula_14ex02/script.js
@@ -0,0 +1,10 @@
+function tabuada(){
+    var num = document.getElementById('txtnum')
+    var n = Number(num.value)
+    var res = document.getElementById('res')
+    var tabuada = ''
+    for (var count = 1; count <=10; count++){
+        tabuada += n +" x "+ count +"= "+ n*count+ "</br>";
+        res.innerHTML = tabuada;
+    }
+}
