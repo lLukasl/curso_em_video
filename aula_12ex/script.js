88d85d8275b765ef9fa3bd221c8d9c88be1bb4a4
Lucas Dias de Miranda <lucas.miranda@pampa.compasso>
2021-02-08 23:59:32 -0300

    Exercicio 12 com mini projeto

---
+++ aula_12ex/script.js
@@ -0,0 +1,20 @@
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if(hora >= 0  && hora < 12){
+        img.src = 'img/fotomanha.jpg'
+        document.body.style.background = '#e2cd9f'
+    }else if(hora >=12 && hora < 18){
+        img.src = 'img/fototarde.jpg'
+        document.body.style.background = '#b9846f'
+    }else{
+        img.src = 'img/fotonoite.jpg'
+        document.body.style.background = '#515154'
+    }
+}
+
+
+
